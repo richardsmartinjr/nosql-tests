8723b1091b4d38392f60481058f7dba9b6be3e46
Justin <leich6@users.noreply.github.com>
2016-04-28 21:21:13 +0000

    Improved tests

---
+++ redis/multi_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+python3 test.py -n a > a.out &
+python3 test.py -n b > b.out &
+python3 test.py -n c > c.out &
+python3 test.py -n d > d.out &
+python3 test.py -n e > e.out &
+python3 test.py -n f > f.out &
+
--- redis/test.py
+++ redis/test.py
@@ -1,6 +1,8 @@
 from random import randint
 import time 
 from rediscluster import StrictRedisCluster
+from rediscluster.exceptions import ClusterError
+from redis.exceptions import ConnectionError
 import argparse
 
 parser = argparse.ArgumentParser()
@@ -12,11 +14,12 @@ name = args.name
 current_milli_time = lambda: int(round(time.time() * 1000))
 
 startup_nodes = [{"host":"10.240.0.8","port":"6379"},{"host":"10.240.0.10","port":"6379"},{"host":"10.240.0.12","port":"6379"}]
-rc = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+r = StrictRedisCluster(startup_nodes=startup_nodes, decode_responses=True)
 
 id = 0
 sessions = {}
 
+deletes = []
 
 #Create 100 sessions
 while id < 200:
@@ -29,38 +32,43 @@ while id < 200:
   id = id + 1
 
 
-while id < 50000:
-  action = randint(1, 3)
-  if action == 1:
-       #Create
-       #Do insert to redis
-       start = current_milli_time()
-       r.set(name + str(id),1)
-       end = current_milli_time()
-       print(str(end-start)+'ms new '+str(id))
-       sessions[id] = 1
-       id = id + 1
-  elif action == 2:
-       #Update
-       k = list(sessions.keys())
-       u = randint(0,len(k)-1)
-       #Insert new value
-       start = current_milli_time()
-       r.set(name + str(k[u]),sessions[k[u]])
-       end = current_milli_time()
-       print(str(end-start)+'ms upd ' + str(k[u]))
-       sessions[k[u]] = sessions[k[u]] + 1 
-  else:
-       #Delete
-       k = list(sessions.keys())
-       u = randint(0,len(k)-1)
-       #Insert into redis
-       start = current_milli_time()
-       r.delete(name + str(k[u]))
-       end = current_milli_time()
-       print(str(end-start)+'ms del ' + str(k[u]))
-       del(sessions[k[u]])
-
+while id < 100000:
+  try:
+    action = randint(1, 3)
+    if action == 1:
+      #Create
+      #Do insert to redis
+      start = current_milli_time()
+      r.set(name + str(id),1)
+      end = current_milli_time()
+      print(str(end-start)+'ms new '+str(id))
+      sessions[id] = 1
+      id = id + 1
+    elif action == 2:
+      #Update
+      k = list(sessions.keys())
+      if len(k):
+        u = randint(0,len(k)-1)
+        #Insert new value
+        start = current_milli_time()
+        r.set(name + str(k[u]),sessions[k[u]] + 1)
+        end = current_milli_time()
+        print(str(end-start)+'ms upd ' + str(k[u]))
+        sessions[k[u]] = sessions[k[u]] + 1 
+    else:
+      #Delete
+      k = list(sessions.keys())
+      if len(k):
+        u = randint(0,len(k)-1)
+        #Insert into redis
+        start = current_milli_time()
+        r.delete(name + str(k[u]))
+        end = current_milli_time()
+        print(str(end-start)+'ms del ' + str(k[u]))
+        deletes.append(k[u])
+        del(sessions[k[u]])
+  except (ClusterError, ConnectionError):
+    print('Cluster Timeout') 
 
 
 #Compare redis sessions to
@@ -68,12 +76,17 @@ for key in sessions.keys():
   a =  sessions[key]
   result = r.get(name + str(key))
   if not result:
-    print('missing ' + str(key))
+    print('error missing ' + str(key))
   else:
-   b = result
+   b = int(result)
    if a != b:
-     print(a+','+b)
+     print('error unequal '+str(a)+','+str(b))
 
+
+for item in deletes:
+  result = r.get(name + str(item))
+  if result:
+    print('error not deleted ' + str(item))
 
 
 
